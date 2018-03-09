f47405a061c20776f41cf9e44e47bc5aaf39704c
shirui Yang <37780012@qq.com>
2018-03-09 14:37:44 +0800

    上传图片事例

---
+++ upimg_pro/config.js
@@ -0,0 +1,17 @@
+/**
+ * 接口配置文件
+ */
+
+var config = {
+
+    host: 'https://ilearnmore.net/',
+    // 图片资源host
+    cdnhost: 'http://img.ilearnmore.net/',
+    shopkey: 'ilearnmore',
+
+    //商家中心小程序appid
+    adminhomeAppId: 'wxb56551754479f0ab',
+
+};
+
+module.exports = config
