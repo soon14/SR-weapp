a1ead4e7cf09cc0d353f8cc4f777f40c13e27acb
shirui Yang <37780012@qq.com>
2018-04-11 23:33:11 +0800

    1

--- expApp/app.js
+++ expApp/app.js
@@ -5,7 +5,12 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const db = require('mongoskin').db('localhost:27017/animals');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoskin = require('mongoskin');
+
+// 创建数据库连接
+const db = mongoskin.connect('mongodb://localhost/myapp');
 
 // 加载路由控制
 const indexRouter = require('./routes/index');
@@ -27,7 +32,9 @@ app.use(logger('dev'));
 
 // 定义数据解析器
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 
 // 定义cookie解析器
 app.use(cookieParser());
@@ -35,23 +42,29 @@ app.use(cookieParser());
 // 定义静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 假如你使用express-session 1.10.0以上版本，你不想用户每次刷新页面都重复往数据库里储存session,你可以通过限制一个时间期限，来lazy更新session。
+app.use(session({
+  secret: 'keyboard cat',
+  saveUninitialized: false, //在存储一些新数据之前，不创建session
+  resave: false, //如果没有发生任何修改不储存session。
+  store: new MongoStore({
+    url: "mongodb://localhost/myapp",
+    touchAfter: 24 * 3600 //单位是秒
+  })
+}));
+
 // 匹配路径和路由
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/a', testRouter);
 
-db.collection('mamals').find().toArray(function(err, result) {
-  if (err) throw err;
-  console.log(result);
-});
-
 // catch 404 and forward to error handler 404错误处理
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler 500错误处理和错误堆栈跟踪
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- expApp/routes/index.js
+++ expApp/routes/index.js
@@ -1,9 +1,18 @@
-const express = require('express');
+const express = require("express");
 let router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//修改router/index.js,第一次请求时我们保存一条用户信息。
+router.get("/", function(req, res, next) {
+  var user = {
+    name: "yangshirui",
+    age: "26",
+    address: "shenzhen"
+  };
+  req.session.user = user;
+  res.render("index", {
+    title: "the test for nodejs session",
+    name: "sessiontest"
+  });
 });
 
 module.exports = router;
